501c75c549f5fb4a88a0c09003eec4600b5a343c
chefinDan <greendan@oregonstate.edu>
2019-01-18 16:34:06 -0800

    reads function arguments

--- sum.sh
+++ sum.sh
@@ -1,14 +1,49 @@
 #!/bin/bash
 
-sum=0
-read myLine
+sum()
+{
 
-echo $myLine
-		
-for i in $myLine
-do
-	echo "i: $i"
-	sum=$(expr $sum + $i)
-done
+	SUM=0
+	while read myLine
+	do
+		echo $myLine
 
-echo "Sum of numbers: $sum"
+		for i in $myLine
+		do
+			echo "i: $i"
+				sum=$(expr $sum + $i)
+		done
+
+	done < $1
+
+		echo "Sum of numbers: $sum"
+		return
+}
+
+# dispArgs()
+# {
+	# CNT=1
+	# for i in $1 $2
+	# do
+		# echo "Param $CNT: $i"
+		# CNT=$(expr $CNT + 1)
+		# echo $@
+		# echo "$1 ${@:2}"
+		# sum $1
+	# done
+	# else
+	# 	echo "Please provide an argument"
+	# 	exit 1
+	# fi
+# }
+
+add()
+{
+	[ "$#" -ne  3 ] && echo "Invalid number of arguments for add function" && exit -1
+}
+
+# Beginning of script
+[ "$#" -lt  2 ] && echo "Invalid number of arguments" && exit -1
+
+# dispArgs $1 $2
+$1 ${@:2}
